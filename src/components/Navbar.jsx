dec6edc98fda63ed418d6a642eddf98c47da82ba
TaimoorQadri123 <kinzaaptech@gmail.com>
2025-07-01 12:51:06 +0500

    React Session 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+      
+      <div className='desktop-navbar bg-slate-900 text-white flex justify-between p-3 items-center'>
+          <div id='navbar-logo' className='text-xl'>
+              <i className="ri-behance-fill"></i>
+          </div>
+          <div id='navbar-items'>
+            <ul className='md:flex hidden gap-5'>
+              <li>Home</li>
+              <li>Categories</li>
+              <li>Products</li>
+            </ul>
+          </div>
+          <div id='navbar-toggle' className='md:invisible visible'>
+            <i className="ri-menu-line"></i>
+          </div>
+      </div>
+
+    </>
   )
 }
 
